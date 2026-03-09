60eb102305e250aff8d6929be656a875a376599f
luchocedres <lucho.cedres.7@gmail.com>
2026-03-09 11:48:16 -0300

    Crear productos.py

---
+++ productos.py
@@ -0,0 +1,17 @@
+# productos.py
+LISTA_PRODUCTOS = [
+    {"id": 1, "nombre": "Detergente Ala", "precio": 950, "habitacion": "cocina", "imagen": "detergente.jpg"},
+    {"id": 2, "nombre": "Jabon Liquido", "precio": 1200, "habitacion": "lavadero", "imagen": "jabon.jpg"},
+    {"id": 3, "nombre": "Pañales G x20", "precio": 4500, "habitacion": "baño", "imagen": "panales.jpg"},
+    {"id": 4, "nombre": "Papel Higienico", "precio": 2500, "habitacion": "baño", "imagen": "papel.jpg"},
+    {"id": 5, "nombre": "Shampoo Plusbelle", "precio": 2100, "habitacion": "baño", "imagen": "shampoo.jpg"},
+    {"id": 6, "nombre": "Perfume Mujer", "precio": 2000, "habitacion": "uso personal", "imagen": "perfume.jpg"},
+    {"id": 7, "nombre": "Desodorante Axe", "precio": 2000, "habitacion": "uso personal", "imagen": "desodorante.jpg"},
+    {"id": 8, "nombre": "Lavandina Ayudin", "precio": 1100, "habitacion": "baño", "imagen": "lavandina.jpg"},
+    {"id": 9, "nombre": "Jabon de ropa Skip", "precio": 2800, "habitacion": "lavadero", "imagen": "jabon_ropa.jpg"},
+    {"id": 10, "nombre": "Cloro 5 Lts", "precio": 4500, "habitacion": "pileta", "imagen": "cloro.jpg"},
+    {"id": 11, "nombre": "Algodon 100g", "precio": 1100, "habitacion": "baño", "imagen": "algodon.jpg"},
+    {"id": 12, "nombre": "Cera Suiza", "precio": 3500, "habitacion": "cocina", "imagen": "cera.jpg"},
+    {"id": 13, "nombre": "Aerosol Glade", "precio": 1800, "habitacion": "habitacion", "imagen": "glade.jpg"},
+    # ... (podes seguir agregando los demas de la lista anterior aca)
+]
